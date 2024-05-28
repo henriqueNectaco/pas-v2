0f35d60c6f6d8427690ec88465013ce48964c23d
henriqueEz4568 <rick.scioli@gmail.com>
2024-05-28 16:32:36 -0300

    la vai bug

--- src/pages/teste/dropteste.tsx
+++ src/pages/teste/dropteste.tsx
@@ -14,6 +14,8 @@ import {
   useDisclosure,
 } from '@nextui-org/react'
 import { DotsThreeOutlineVertical } from 'phosphor-react'
+import DateRangePickerComponent from '@/components/rangedatepicker'
+import Mteste from './modal'
 type TypeProps = {
   id: number
 }
@@ -62,45 +64,7 @@ export default function DropdownButton(props: TypeProps) {
           <DropdownItem key="turnOff">Desativar</DropdownItem>
         </DropdownMenu>
       </Dropdown>
-      <Modal isOpen={isOpen} onOpenChange={onOpenChange}>
-        <ModalContent>
-          {(onClose) => (
-            <>
-              <ModalHeader className="flex flex-col gap-1">
-                Modal Title
-              </ModalHeader>
-              <ModalBody>
-                <p>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                  Nullam pulvinar risus non risus hendrerit venenatis.
-                  Pellentesque sit amet hendrerit risus, sed porttitor quam.
-                </p>
-                <p>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                  Nullam pulvinar risus non risus hendrerit venenatis.
-                  Pellentesque sit amet hendrerit risus, sed porttitor quam.
-                </p>
-                <p>
-                  Magna exercitation reprehenderit magna aute tempor cupidatat
-                  consequat elit dolor adipisicing. Mollit dolor eiusmod sunt ex
-                  incididunt cillum quis. Velit duis sit officia eiusmod Lorem
-                  aliqua enim laboris do dolor eiusmod. Et mollit incididunt
-                  nisi consectetur esse laborum eiusmod pariatur proident Lorem
-                  eiusmod et. Culpa deserunt nostrud ad veniam.
-                </p>
-              </ModalBody>
-              <ModalFooter>
-                <Button color="danger" variant="light" onPress={onClose}>
-                  Close
-                </Button>
-                <Button color="primary" onPress={onClose}>
-                  Action
-                </Button>
-              </ModalFooter>
-            </>
-          )}
-        </ModalContent>
-      </Modal>
+      <Mteste isOpen={isOpen} onClose={onclose} onOpenChange={onOpenChange} />
     </>
   )
 }
---
+++ src/pages/teste/modal.tsx
@@ -0,0 +1,52 @@
+import DateRangePickerComponent from '@/components/rangedatepicker'
+import {
+  Modal,
+  ModalContent,
+  ModalHeader,
+  ModalBody,
+  ModalFooter,
+  Button,
+} from '@nextui-org/react'
+type typeProps = {
+  onClose: any
+  isOpen: any
+  onOpenChange: any
+}
+export default function Mteste(props: typeProps) {
+  return (
+    <>
+      <Modal
+        isOpen={props.isOpen}
+        onOpenChange={props.onOpenChange}
+        placement="top"
+      >
+        <ModalContent>
+          {(onClose) => (
+            <>
+              <ModalHeader className="flex flex-col items-center justify-center gap-1">
+                Selecione um intervalo
+              </ModalHeader>
+              <ModalBody className="flex flex-col items-center justify-center lg:p-12">
+                <DateRangePickerComponent />
+              </ModalBody>
+              <ModalFooter className="flex flex-row items-center justify-center">
+                <Button color="danger" variant="light" onPress={props.onClose}>
+                  Cancelar
+                </Button>
+                <Button
+                  color="primary"
+                  onPress={() => {
+                    alert('confirmado')
+                    props.onClose()
+                  }}
+                >
+                  Confirmar
+                </Button>
+              </ModalFooter>
+            </>
+          )}
+        </ModalContent>
+      </Modal>
+    </>
+  )
+}
