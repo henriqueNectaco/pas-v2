6b5c504aed9f5b33139638600e7e6f3e71f0e267
henriqueNectaco <henrique@nectaco.com.br>
2024-07-30 18:40:55 -0500

    vazando

--- src/components/newHeader/index.tsx
+++ src/components/newHeader/index.tsx
@@ -20,57 +20,58 @@ export default function NewHeader() {
   const { isOpen, onOpen, onOpenChange } = useDisclosure()
   const showSiderbar = () => setSidebar(!sidebar)
 
-  return (<>
-    <div className={`  bg-blue-600 grid grid-cols-5 shadow-[0_0_7px_3px] `}>
-      <div className=' text-white  flex items-center p-6 col-span-1'><TextAlignJustify size={40} onClick={showSiderbar} className='hover:cursor-pointer' /></div>
-      <div className='col-span-3 border flex flex-row p-6 items-center gap-4 justify-center'>
-        <Link href={`${process.env.NEXT_PUBLIC_BASE_URL}/dashboard`}>Dashboard</Link>
-        <Link href={`${process.env.NEXT_PUBLIC_BASE_URL}/vendas`}>Vendas</Link>
-        <Link href={`${process.env.NEXT_PUBLIC_BASE_URL}/marketplaces`}>Marketplaces</Link>
-        <Link href={`${process.env.NEXT_PUBLIC_BASE_URL}/crons`}>Crons</Link>
-      </div>
-      <div className=' flex col-span-1 items-center  justify-end p-6'>
-        <SignOut color='white' size={32} className='hover:cursor-pointer' onClick={onOpen} />
-        {/* <UserCircle color='white' size={32} className='hover:cursor-pointer' onClick={onOpen} /> */}
-      </div>
-    </div>
-    {sidebar && <Sidebar active={setSidebar} />}
+  return (
     <>
-      <Modal
-        isOpen={isOpen}
-        //          placement={modalPlacement}
-        placement='top'
-        onOpenChange={onOpenChange}
-        radius='md'
-      >
-        <ModalContent className=" border-2 lg:w-1/4 w-2/3 md:w-2/4">
-          {(onClose) => (
-            <>
-              <ModalHeader className="  flex flex-col  p-4 items-start justify-center gap-1">
-                <p className="text-yellow-400">LogOut</p>
-              </ModalHeader>
-              <ModalBody className=" flex flex-col items-center justify-center  border-b border-black p-6">
-                <p className="text-xl">Deseja Efetuar o LogOut?</p>
-              </ModalBody>
-              <ModalFooter className="flex p-4 items-center justify-center">
+      <div className={`  bg-blue-600 flex flex-row lg:grid lg:grid-cols-5 shadow-[0_0_7px_3px] `}>
+        <div className=' text-white  flex items-center p-6 lg:col-span-1 w-full'><TextAlignJustify size={40} onClick={showSiderbar} className='hover:cursor-pointer' /></div>
+        <div className='hidden col-span-3  lg:flex flex-row p-6 items-center gap-6 justify-center'>
+          <Link href={`${process.env.NEXT_PUBLIC_BASE_URL}/dashboard`} className='text-lg'>Dashboard</Link>
+          <Link href={`${process.env.NEXT_PUBLIC_BASE_URL}/vendas`} className='text-lg'>Vendas</Link>
+          <Link href={`${process.env.NEXT_PUBLIC_BASE_URL}/marketplaces`} className='text-lg'>Marketplaces</Link>
+          <Link href={`${process.env.NEXT_PUBLIC_BASE_URL}/crons`} className='text-lg'>Crons</Link>
+        </div>
+        <div className=' flex col-span-1 items-center  justify-end  w-full  p-6'>
+          <SignOut color='white' size={32} className='hover:cursor-pointer' onClick={onOpen} />
+          {/* <UserCircle color='white' size={32} className='hover:cursor-pointer' onClick={onOpen} /> */}
+        </div>
+      </div>
+      {sidebar && <Sidebar active={setSidebar} />}
+      <>
+        <Modal
+          isOpen={isOpen}
+          //          placement={modalPlacement}
+          placement='top'
+          onOpenChange={onOpenChange}
+          radius='md'
+        >
+          <ModalContent className=" border-2 lg:w-1/4 w-2/3 md:w-2/4">
+            {(onClose) => (
+              <>
+                <ModalHeader className="  flex flex-col  p-4 items-start justify-center gap-1">
+                  <p className="text-yellow-400">LogOut</p>
+                </ModalHeader>
+                <ModalBody className=" flex flex-col items-center justify-center  border-b border-black p-6">
+                  <p className="text-xl">Deseja Efetuar o LogOut?</p>
+                </ModalBody>
+                <ModalFooter className="flex p-4 items-center justify-center">
 
-                <Button
-                  radius='sm'
-                  fullWidth={true}
-                  size="md"
-                  color="primary"
-                  variant="solid"
-                  onPress={LogOut}
-                >
-                  Confirmar
-                </Button>
-              </ModalFooter>
-            </>
-          )}
-        </ModalContent>
-      </Modal>
+                  <Button
+                    radius='sm'
+                    fullWidth={true}
+                    size="md"
+                    color="primary"
+                    variant="solid"
+                    onPress={LogOut}
+                  >
+                    Confirmar
+                  </Button>
+                </ModalFooter>
+              </>
+            )}
+          </ModalContent>
+        </Modal>
+      </>
     </>
-  </>
 
   )
 }
